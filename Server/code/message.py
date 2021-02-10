bbcb25889ee4fc671d1f07c3f6a302eadc79d028
GiacomoPauletti <giacomo.pauletti@gmail.com>
2021-02-10 19:05:30 +0100

    Divided Server main code into modules
    In Server/code/: Divided main code in several modules: user.py for User (and related stuff), chat.py for Chat (...), message.py for Message (...). Created utilities/ for low level / support modules. Created utilities/shared_abcs.py to contain all abstract classes sharable between many modules

---
+++ Server/code/message.py
@@ -0,0 +1,51 @@
+import abc
+
+class Message:
+    @classmethod
+    def _fromString(cls, string : str):
+        closing_header_index=string.index('>')
+        header=string[1:closing_header_index]
+        content=string[closing_header_index+1:]
+
+        return cls(header=header, content=content)
+
+    @classmethod
+    def _fromTuple(cls, tpl : tuple):
+        if len(tpl) != 2:
+            return None
+        return cls(header=tpl[0], content=tpl[1])
+
+    @classmethod
+    def _fromDict(cls, dictionary : dict):
+        return cls(header=dictionary['header'], content=dictionary['content'])
+
+    def __init__(self, header : str, content : str):
+        self.__header = header
+        self.__content = content
+
+    def getHeader(self):
+        return self.__header
+    
+    def getContent(self):
+        return self.__content
+
+    def __str__(self):
+        return f'<{self.__header}>{self.__content}'
+
+
+class IMessageFactory(abc.ABC):
+    @abc.abstractmethod
+    def createMessage(self):
+        return Message()
+
+class FromStringMessageFactory(IMessageFactory):
+    def createMessage(self, string : str):
+        return Message._fromString(string)
+
+class FromTupleMessageFactory(IMessageFactory):
+    def createMessage(self, tpl : tuple):
+        return Message._fromTuple(tpl)
+
+class FromDictMessageFactory(IMessageFactory):
+    def createMessage(self, dictionary : dict):
+        return Message._fromDict(dictionary)
---
+++ Server/code/utilities/shared_abcs.py
@@ -0,0 +1,17 @@
+import abc
+
+class IObserver(abc.ABC):
+    @abc.abstractmethod
+    def update(self):
+        ...
+
+class IObservable(abc.ABC):
+    @abc.abstractmethod
+    def registerObserver(self, observer : IObserver) -> None:
+        ...
+
+    def removeObserver(self, observer : IObserver) -> None:
+        ...
+
+    def notifyObservers(self) -> None:
+        ...
