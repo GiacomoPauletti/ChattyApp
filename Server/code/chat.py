b941987f927231d15c554e6dd1e0159a698b75c0
GiacomoPauletti <giacomo.pauletti@gmail.com>
2021-03-02 19:43:08 +0100

    Edited Chatid. It can now convert a string into a Chatid type

--- Server/code/chat.py
+++ Server/code/chat.py
@@ -1,9 +1,14 @@
 from utilities.abcs import IObserver, IObservable
 
-class Chatid
+class Chatid:
+    
+    @classmethod
+    def from_string(cls, string : str):
+        return cls(value=string)
+
     def __init__(self, value : str):
         self.__value=value
-    
+
     def getValue(self):
         return self.__value
 
