0ed304262e21c3e1f7009f0988bfd75be18631b7
GiacomoPauletti <giacomo.pauletti@gmail.com>
2021-03-07 16:18:59 +0100

    Server: first update to main

--- Server/code/main.py
+++ Server/code/main.py
@@ -1,12 +1,22 @@
 import abc
 import threading
-from typing import List
 import socket
 
-from message import Message
-from chat import Chat
-from user import User
+from message.abcs import Message
+from chat.chat import Chat
+from user.user import User
+from user.user_listeners import UnaccessedUserListener, AccessedUserListener
+from handlers.access_handler import AccessHandler
+from storage.abcs import UserLogger, UserRegister
+from utilities.registers import AuthorizedUserRegister
+
+global_user_logger=UserLogger()
+global_user_register=UserRegister()
+
+auth_users=AuthorizedUserRegister()
+accesshandler=AccessHandler(global_user_logger, global_user_register, Message, Message, auth_users)
 
 
-
+if __name__ == "__main__":
+    ...
 
