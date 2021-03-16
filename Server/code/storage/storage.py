f55b5bebc2c53dee86ecb0f8cd293298876eec14
GiacomoPauletti <giacomo.pauletti@gmail.com>
2021-03-16 18:53:50 +0100

    Server: created init_db.py and text_init_db

--- Server/code/storage/storage.py
+++ Server/code/storage/storage.py
@@ -4,30 +4,22 @@ from utilities.chatid import Chatid
 from user.abcs import User
 import os
 
-class DatabaseConstructor(abc.ABC):
-    @abc.abstractmethod
-    def make_chat_db(self, chatid : Chatid) -> None:
-        ...
-
-    @abc.abstractmethod
-    def make_user_db(self, private_name : str) -> None:
-        ...
-
-class TextDatabaseConstructor(DatabaseConstructor):
-    def make_chat_db(self, chatid : Chatid) -> None:
-        ...
-
-    def make_user_db(self, private_name : str) -> None:
-        ...
-
             
 class UserStorage(abc.ABC):
     @abc.abstractmethod
-    def get_user_chats(self, private_name : str) -> dict:
+    def new_user(self, private_name : str):
         ...
 
     @abc.abstractmethod
-    def get_user_unread_messages(self, private_name : str, chat=None) -> Message:
+    def get_chats(self, private_name : str) -> dict:
+        ...
+
+    @abc.abstractmethod
+    def get_notifications(self, private_name : str) -> str:
+        ...
+
+    @abc.abstractmethod
+    def get_unread_messages(self, private_name : str, chat=None) -> Message:
         #si potrebbe fare che ritorna un iterator o che funziona da iterator
         #così da dare un messaggio alla volta
         ...
@@ -36,14 +28,24 @@ class TextUserStorage(abc.ABC):
     def __init__(self):
         pass
 
-    def get_user_chats(self, private_name : str) -> dict:
+    def new_user(self, private_name : str):
         pass
 
-    def get_user_unread_messages(self, private_name : str, chat=None) -> Message:
+    def get_chats(self, private_name : str) -> dict:
+        pass
+
+    def get_notifications(self, private_name : str) -> str:
+        ...
+
+    def get_unread_messages(self, private_name : str, chat: Chatid) -> Message:
         pass
 
 
 class ChatStorage(abc.ABC):
+    @abc.abstractmethod
+    def new_chat(self, chatid : Chatid):
+        ...
+
     @abc.abstractmethod
     def get_info(self, chatid : Chatid):
         ...
