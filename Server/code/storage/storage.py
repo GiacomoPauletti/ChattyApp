7ca816153f91274b80852c21c891eefca85398f7
GiacomoPauletti <giacomo.pauletti@gmail.com>
2021-03-23 14:25:34 +0100

    Server: divided ChatStorage in two sublcasses
    Subclasses are: MessageStorage and UserChatStorage

--- Server/code/storage/storage.py
+++ Server/code/storage/storage.py
@@ -278,7 +278,193 @@ class UserStorage:
         self.__notification_storage.remove(private_name, end)
 
 
+class UserChatStorage(abc.ABC):
 
+    @abc.abstractmethod
+    def _new_chat(self, chatid : Chatid):
+        ...
+
+    @abc.abstractmethod
+    def add_user(self, chatid : Chatid, private_name : str):
+        ...
+
+    @abc.abstractmethod
+    def get_indexes(self, chatid : Chatid):
+        ...
+
+    @abc.abstractmethod
+    def get_user_index(self, chatid : Chatid, private_name : str):
+        ...
+
+    @abc.abstractmethod
+    def increment_user_index(self, chatid : Chatid, private_name : str, incrementor=1) -> None:
+        ...
+
+    @abc.abstractmethod 
+    def get_maximum_index(self, chatid : Chatid) -> int:
+        ...
+
+class TextUserChatStorage(UserChatStorage):
+    def __init__(self, default_path='./database/chats'):
+        self.__default_path=default_path
+
+    def is_chat_existing(self, chatid : Chatid) -> bool:
+        all_chats=os.walk(self.__default_path).__next__()[1]  
+
+        is_chat_existing=str(chatid) in all_chats 
+        if not is_chat_existing: 
+            print(f'[ChatStorage] the chat {str(chatid)} is not existing') 
+            return False 
+
+        return True 
+
+    def add_user(self, chatid : Chatid, private_name : str):
+
+        if not self.is_chat_existing(chatid):
+            return None
+
+        user_chat_path=self.__default_path + f'/{str(chatid)}/users.txt'
+        
+        for user, index in self.get_indexes(chatid): 
+            if user == private_name:
+                print(f'[ChatStorage] the user {private_name} has already been added to {str(chatid)}')
+                return None
+
+        open(user_chat_path, 'a').write(f'{private_name}:0\n')
+
+    def get_indexes(self, chatid : Chatid):
+        """
+        TextUserChatStorage.get_indexes(self, chatid)
+
+        It is an iterator which yields the user and the respective index of the passed chat, if it exists
+        """
+
+        if not self.is_chat_existing(chatid):
+            return None
+
+        user_chat_path=self.__default_path + f'/{str(chatid)}/users.txt'
+
+        lines=open(user_chat_path, 'r').readlines()
+        if len(lines)==0:
+            return None
+
+        users_indexes={user:int(index) for user,index in [(line.split(':')) for line in lines]}
+        
+        for user, index in users_indexes.items():
+            yield (user, index)
+    
+    def get_user_index(self, chatid : Chatid, private_name : str):
+        """
+        TextUserChatStorage.get_user_index(eslf, chatid, private_name)
+
+        It returns the index of the last message read/received by the user
+        """
+
+        if not self.is_chat_existing(chatid):
+            return None
+
+        for user, index in self.get_indexes(chatid):
+            if user==private_name:
+                return index
+        else:
+            return None
+
+    def increment_user_index(self, chatid : Chatid, private_name : str, incrementor=1) -> None:
+        """
+        TextUserChatStorage.increment_user_index(self, chatid, private_name)
+
+        It increments by 'incrementor' the index of the passed user
+        """ 
+
+        user_indexes={user:index for user, index in self.get_indexes(chatid)}
+        if not user_indexes:
+            return None
+        
+        if not private_name in user_indexes.keys():
+            return None
+
+        user_indexes[private_name] += incrementor
+
+        user_chat_path=self.__default_path + f'/{str(chatid)}/users.txt'
+        with open(user_chat_path, 'w') as f:
+            for user, index in user_indexes.items():
+                f.write(f'{user}:{index}\n')
+
+    def get_maximum_index(self, chatid : Chatid) -> int:
+        """
+        TextUserChatStorage.get_maximum_index(self, chatid)
+
+        It returns the index of the most recent message of the chat, which is the maiximum index.
+        The passed chat must exist already
+        """
+
+        if not self.is_chat_existing(chatid):
+            return None
+
+        messages_chat_path=self.__default_path+f'/{str(chatid)}/messages.txt'        
+        return _get_num_of_lines(messages_chat_path)
+
+class MessageStorage(abc.ABC):
+    @abc.abstractmethod
+    def _new_chat(self, chatid : Chatid):
+        ...
+
+    @abc.abstractmethod
+    def add_message(self, chatid : Chatid, message : Message):
+        ...
+
+    @abc.abstractmethod
+    def get_messages(self, chatid : Chatid, start : int, end : int) -> Message:
+        ...
+
+class TextMessageStorage(MessageStorage):
+    def __init__(self, chat_message_class, default_path='./database/chats'):
+        self.__ChatMessage=chat_message_class
+        
+        self.__default_path=default_path
+
+    def _new_chat(self, chatid: Chatid):
+        new_chat_path=self.__default_path+f'/{str(chatid)}'
+        os.mkdir(new_chat_path)
+
+        open(new_chat_path + f'/messages.txt', 'w')
+
+    def is_chat_existing(self, chatid : Chatid) -> bool:
+        all_chats=os.walk(self.__default_path).__next__()[1]  
+
+        is_chat_existing=str(chatid) in all_chats 
+        if not is_chat_existing: 
+            print(f'[ChatStorage] the chat {str(chatid)} is not existing') 
+            return False 
+
+        return True 
+
+    def add_message(self, chatid : Chatid, message : Message):
+
+        if not self.is_chat_existing(chatid):
+            return None
+
+        messages_chat_path=self.__default_path + f'/{chatid}/messages.txt'
+        with open(messages_chat_path, 'a') as msg_file:
+            msg_file.write(f'{str(message)}\n')
+
+    def get_messages(self, chatid : Chatid, start : int, end : int) -> Message:
+        if not self.is_chat_existing(chatid):
+            return None
+        
+        if end < start or start < 0:
+            return None
+        
+        end = end if end < self.get_maximum_index(chatid) else self.get_maximum_index(chatid)
+
+        messages_chat_path=self.__default_path+f'/{str(chatid)}/messages.txt'
+        with open(messages_chat_path, 'r') as f:
+            for index, message in enumerate(f):
+                if start <= index <= end:
+                    message.rstrip('\n')
+                    final_message=self.__ChatMessage.from_string(message)
+                    yield final_message
+"""
 class ChatStorage(abc.ABC):
     @abc.abstractmethod
     def new_chat(self, chatid : Chatid):
@@ -328,11 +514,9 @@ class TextChatStorage(ChatStorage):
         self.__ChatMessage=chat_message_class
 
     def new_chat(self, chatid : Chatid):
-        """
         TextChatStorage.new_chat(self, chatid)
 
         It creates the database for a new chat, if it hasn't been created yet
-        """
 
         if self.is_chat_existing(chatid):
             return None
@@ -356,11 +540,9 @@ class TextChatStorage(ChatStorage):
         return True 
 
     def add_user(self, chatid : Chatid, private_name : str):
-        """
         TextChatStorage.add_user(self, chatid, private_name)
 
         It adds the user whose private_name is passed to the passed chat database, if both existing
-        """
     
         if not self.is_chat_existing(chatid):
             return None
@@ -375,11 +557,9 @@ class TextChatStorage(ChatStorage):
         open(user_chat_path, 'a').write(f'{private_name}:0\n')
 
     def get_indexes(self, chatid : Chatid):
-        """
         TextChatStorage.get_indexes(self, chatid)
 
         It is an iterator which yields the user and the respective index of the passed chat, if it exists
-        """
 
         if not self.is_chat_existing(chatid):
             return None
@@ -396,11 +576,9 @@ class TextChatStorage(ChatStorage):
             yield (user, index)
 
     def get_user_index(self, chatid : Chatid, private_name : str):
-        """
         TextChatStorage.get_user_index(eslf, chatid, private_name)
 
         It returns the index of the last message read/received by the user
-        """
 
         if not self.is_chat_existing(chatid):
             return None
@@ -413,11 +591,9 @@ class TextChatStorage(ChatStorage):
 
 
     def increment_user_index(self, chatid : Chatid, private_name : str, incrementor=1) -> None:
-        """
         TextChatStorage.increment_user_index(self, chatid, private_name)
 
         It increments by 'incrementor' the index of the passed user
-        """ 
 
         user_indexes={user:index for user, index in self.get_indexes(chatid)}
         if not user_indexes:
@@ -436,12 +612,10 @@ class TextChatStorage(ChatStorage):
 
 
     def get_maximum_index(self, chatid : Chatid) -> int:
-        """
         TextChatStorage.get_maximum_index(self, chatid)
 
         It returns the index of the most recent message of the chat, which is the maiximum index.
         The passed chat must exist already
-        """
 
         if not self.is_chat_existing(chatid):
             return None
@@ -459,13 +633,11 @@ class TextChatStorage(ChatStorage):
             msg_file.write(f'{str(message)}\n')
 
     def get_messages(self, chatid : Chatid, start : int, end : int) -> Message:
-        """
         TextChatStorage.get_messages(self, chatid, start, end) 
 
         It is an iterator which yields the messages between the start and end of the passed chat (if it exists)
         
         Both messages at index 'start' and 'end' are yielded
-        """ 
 
         if not self.is_chat_existing(chatid):
             return None
@@ -484,7 +656,6 @@ class TextChatStorage(ChatStorage):
                     yield final_message
 
 
-"""
 class UserStorage(abc.ABC):
     @abc.abstractmethod
     def new_user(self, private_name : str):
