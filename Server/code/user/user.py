faf13573fe5cdf727cebe517eb5f036adf2af6a0
GiacomoPauletti <giacomo.pauletti@gmail.com>
2021-03-31 20:09:28 +0200

    Server: wrapped user_loop in a class
    In this way it can be stopped calling the .stop() method of the class

--- Server/code/user/user.py
+++ Server/code/user/user.py
@@ -20,15 +20,16 @@ def init_user(private_name, client, client_address):
     server_user=user_factory(private_name)
     remote_user_proxy=remote_user_proxy_factory(client, client_address)
 
-    user_loop_thread=threading.Thread(target=user_loop, args=(server_user, remote_user_proxy))
-    user_loop_thread.start()
-    
+    user_loop=UserLoop(server_user, remote_user_proxy)
+    user_loop.start()
 
+    
 class User(IObserver, IObservable):     
     def __init__(self, private_name):
         self.__chats={}
         self.__private_name=private_name
         self.__new_messages=[]
+
 
     def register_chat(self, chat : Chat) -> None:
         """Part of the Observer pattern (Observable)
@@ -59,6 +60,11 @@ class User(IObserver, IObservable):
 
         self.__new_messages.append(message)
 
+    def receive_unread_messages(self):
+        for chat in self.__chats:
+            for message in chat.send_unread_messages(self.__private_name):
+                self.__messages.append(message)
+
     def get_new_messages(self):
         """ iterate through all messages of the user"""
         for message in self.__new_messages:
@@ -70,6 +76,7 @@ class User(IObserver, IObservable):
             message=self.__new_messages[0]
             yield message
             self.__new_messages.pop(0)
+
 
 class UserRemoteProxy:
     def __init__(self, client, client_address):
@@ -84,18 +91,56 @@ class UserRemoteProxy:
         message=msg.from_string()
         return message
 
+class UserLoop:
+    def __init__(self, server_user, user_remote_proxy, sleep=0):
+        self.__user=user
+        self.__user_remote_proxy=user_remote_proxy
+        self.__sleep=sleep
 
-def user_loop(server_user, user_remote_proxy):
-    """Handles the messages of the remote user that must be sent a certain chat and viceversa"""
+        self.__is_active=False
 
-    remote_user_message = user_remote_proxy.receive_from_remote()
-
-    is_chat_message = remote_user_message != USER_TICK_MESSAGE
-    if is_chat_message:
-        #WARNING: ChatMessage è hard coded, quindi questa funzione è strictly coupled con quella classe
-        message4chat=ChatMessage.from_string(remote_user_message)
+    def start(self):
+        loop_thread=threading.Thread(target=self._loop)
+        loop_thread.start()
         
-        server_user.send_message(message4chat)
+    def _loop(self):
+        """Handles the messages of the remote user that must be sent a certain chat and viceversa"""
 
-    for new_message in server_user.pop_new_messages():
-        user_remote_proxy.send_to_remote(new_message) 
+        server_user.receive_unread_messages()
+
+        self.__is_active=True
+        while self.__is_active:
+            remote_user_message = user_remote_proxy.receive_from_remote()
+
+            is_chat_message = remote_user_message != USER_TICK_MESSAGE
+            if is_chat_message:
+                #WARNING: ChatMessage è hard coded, quindi questa funzione è strictly coupled con quella classe
+                message4chat=ChatMessage.from_string(remote_user_message)
+                
+                server_user.send_message(message4chat)
+
+            for new_message in server_user.pop_new_messages():
+                user_remote_proxy.send_to_remote(new_message) 
+
+    def stop(self):
+        self.__is_active=False
+
+"""
+def user_loop(server_user, user_remote_proxy, sleep=0):  #implementare sleep (il thread va in sleep tra un loop e l'altro per n tempo)
+    #Handles the messages of the remote user that must be sent a certain chat and viceversa
+
+    server_user.receive_unread_messages()
+
+    while True:
+        remote_user_message = user_remote_proxy.receive_from_remote()
+
+        is_chat_message = remote_user_message != USER_TICK_MESSAGE
+        if is_chat_message:
+            #WARNING: ChatMessage è hard coded, quindi questa funzione è strictly coupled con quella classe
+            message4chat=ChatMessage.from_string(remote_user_message)
+            
+            server_user.send_message(message4chat)
+
+        for new_message in server_user.pop_new_messages():
+            user_remote_proxy.send_to_remote(new_message) 
+"""
