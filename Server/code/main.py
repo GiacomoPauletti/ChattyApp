9ae82bf40cbc341d1687bd76288c575f69d2b6b5
GiacomoPauletti <giacomo.pauletti@gmail.com>
2021-04-15 18:06:16 +0200

    Server: updated main.py
    added the inizialization of chat_handler and chat_handler_server

--- Server/code/main.py
+++ Server/code/main.py
@@ -7,6 +7,8 @@ from chat.chat import Chat
 from user.user import User, get_text_user_initializator
 from user.user_listeners import UnauthUserListener, AuthUserListener
 from handlers.access_handler import text_access_handler_factory
+#from handlers.notification_handler import text_notification_handler_factory
+from handlers.chat_handler import text_chat_handler_factory, ChatHandlerServer
 from storage.chat_storage import TextUserChatStorage
 import utilities.registers as rgs
 
@@ -15,18 +17,25 @@ auth_user_register=rgs.AuthorizedUserRegister()
 active_user_register=rgs.ActiveUserRegister()
 active_chat_register=rgs.ActiveChatRegister(active_user_register, TextUserChatStorage(), Chat)
 
-#access handler initialization
-access_handler=text_access_handler_factory(user_message_class=AccessMessage, answer_message_class=AccessAnswerMessage, authorized_user_register=auth_user_register)
+#handlers initialization
+access_handler=text_access_handler_factory(authorized_user_register=auth_user_register)
+chat_handler=text_chat_handler_factory(active_user_register, active_chat_register)
 
-#auth and unauth user listener initialization
+#servers initialization
 unauth_user_listener=UnauthUserListener(access_handler)
+
 user_initializator=get_text_user_initializator(active_user_register, active_chat_register)
 auth_user_listener=AuthUserListener(auth_user_register, user_initializator)
 
+chat_handler_server=ChatHandlerServer(chat_handler)
+
+
 if __name__ == "__main__":
 
+    #servers listening
     unauth_user_listener.listen()
     auth_user_listener.listen()
+    chat_handler_server.listen()
     
     is_online=True
     while is_online:
