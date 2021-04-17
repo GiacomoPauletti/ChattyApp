056a351261e5ce8d242ef1df6c28cf1983527810
GiacomoPauletti <giacomo.pauletti@gmail.com>
2021-04-17 21:35:37 +0200

    Server: implemented ChatRequestMessage in user.py

--- Server/code/user/user.py
+++ Server/code/user/user.py
@@ -1,7 +1,4 @@
-"""import sys
-sys.path.append("utilities")"""
-
-import threading
+import threading, time
 
 from utilities.shared_abcs import IObserver, IObservable
 from utilities.registers import AuthorizedUserRegister
@@ -115,11 +112,16 @@ class UserRemoteProxy:
         return message
 
 class UserLoop:
-    def __init__(self, server_user, user_remote_proxy, sleep=0, chat_message_class=msg.ChatMessage):
+    def __init__(self, server_user, user_remote_proxy, sleep=0, chat_request_message_class=msg.ChatRequestMessage, chat_message_class=msg.ChatMessage, timeout=60):
         self.__server_user=server_user
         self.__user_remote_proxy=user_remote_proxy
         self.__sleep=sleep
+        self.__ChatRequestMessage=chat_request_message_class
         self.__ChatMessage=chat_message_class
+
+        self.__timeout
+
+        self.__user_action_map={'chat':self.chat, 'tick':self.tick, 'disconnect':self.disconnect}
 
         self.__is_active=False
 
@@ -130,26 +132,44 @@ class UserLoop:
     def _loop(self):
         """Handles the messages of the remote user that must be sent a certain chat and viceversa"""
 
+        self.__start_=time.time() 
+
         self.__server_user.receive_unread_messages()
 
         self.__is_active=True
         while self.__is_active:
 
+            now=time.time()
+            if now - self.__start_ >= self.__timeout:
+                #+ send disconnection message to user
+                print('[UserLoop] inactive user: disconnection')
+                self.stop()
+
             for new_message in self.__server_user.pop_new_messages():
                 self.__user_remote_proxy.send_to_remote(new_message) 
 
             remote_user_message = self.__user_remote_proxy.receive_from_remote()
+            user_request=self.__ChatRequestMessage.from_string(remote_user_message)
 
-            is_chat_message = remote_user_message != USER_TICK_MESSAGE
-            if is_chat_message:
-                #WARNING: ChatMessage è hard coded, quindi questa funzione è strictly coupled con quella classe
-                message4chat=self.__ChatMessage.from_string(remote_user_message)
-                
-                self.__server_user.send_message(message4chat)
+            self.__user_action_map[user_request.get_action()](user_request)
+        print('[UserLoop] disconnected from inactive user')
+        return None
 
 
     def stop(self):
         self.__is_active=False
+
+    def chat(self, user_request):
+        chat_message=user_request.get_message()
+        self.__server_user.send_message(chat_message)
+
+    def tick(self, user_request):
+        self.__start_=time.time()
+
+    def disconnect(self, user_request):
+        self.stop()
+        
+
 
 """
 def user_loop(server_user, user_remote_proxy, sleep=0):  #implementare sleep (il thread va in sleep tra un loop e l'altro per n tempo)
